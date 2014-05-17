04a4bbc56f327c4e44778cbe4c187e7a0a1dbb81
Moss Collum <moss.collum@gmail.com>
2014-05-16 20:01:39 -0400

    Extract module: GameMap

--- Game.hs
+++ Game.hs
@@ -2,6 +2,7 @@ module Game where
 
 import qualified Data.Map.Lazy as Map
 
+import GameMap
 import Roguelike
 
 data GameState = GameState { gsMap :: GameMap
@@ -9,29 +10,11 @@ data GameState = GameState { gsMap :: GameMap
                            , gsOrc :: Position
                            }
                | QuitGame
-type GameMap = Map.Map Position Terrain
-data Terrain = NoTerrain | Floor | Wall
-             deriving (Eq)
 
 newGame = GameState { gsMap = mapWithRoom (35,7) (44,16)
                     , gsPlayer = (42, 9)
                     , gsOrc = (37, 14)
                     }
-
-mapWithRoom :: Position -> Position -> GameMap
-mapWithRoom (left,top) (right,bottom) = Map.fromList $
-    [(pos, Floor) | pos <- positionsInRange (left,top) (right,bottom)]
-    ++ [(pos, Wall) | pos <- positionsInRange (left-1,top) (left-1,bottom)]
-    ++ [(pos, Wall) | pos <- positionsInRange (right+1,top) (right+1,bottom)]
-    ++ [(pos, Wall) | pos <- positionsInRange (left-1,top-1) (right+1,top-1)]
-    ++ [(pos, Wall) | pos <- positionsInRange (left-1,bottom+1) (right+1,bottom+1)]
-
-positionsInRange :: Position -> Position -> [Position]
-positionsInRange (left,top) (right,bottom) =
-    [(x,y) | x <- [left..right], y <- [top..bottom]]
-
-terrainAt :: Position -> GameMap -> Terrain
-terrainAt = Map.findWithDefault NoTerrain
 
 instance Roguelike GameState where
     advance gameState inputCharacter = let advancePlayer = getCommand inputCharacter
---
+++ GameMap.hs
@@ -0,0 +1,24 @@
+module GameMap ( GameMap, Terrain(NoTerrain,Floor,Wall), mapWithRoom, terrainAt ) where
+
+import qualified Data.Map.Lazy as Map
+
+import Roguelike
+
+type GameMap = Map.Map Position Terrain
+data Terrain = NoTerrain | Floor | Wall
+             deriving (Eq)
+
+mapWithRoom :: Position -> Position -> GameMap
+mapWithRoom (left,top) (right,bottom) = Map.fromList $
+    [(pos, Floor) | pos <- positionsInRange (left,top) (right,bottom)]
+    ++ [(pos, Wall) | pos <- positionsInRange (left-1,top) (left-1,bottom)]
+    ++ [(pos, Wall) | pos <- positionsInRange (right+1,top) (right+1,bottom)]
+    ++ [(pos, Wall) | pos <- positionsInRange (left-1,top-1) (right+1,top-1)]
+    ++ [(pos, Wall) | pos <- positionsInRange (left-1,bottom+1) (right+1,bottom+1)]
+
+positionsInRange :: Position -> Position -> [Position]
+positionsInRange (left,top) (right,bottom) =
+    [(x,y) | x <- [left..right], y <- [top..bottom]]
+
+terrainAt :: Position -> GameMap -> Terrain
+terrainAt = Map.findWithDefault NoTerrain
