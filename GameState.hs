978b7c142b071a4c5412227d7048ba656c504749
Moss Collum <moss.collum@gmail.com>
2014-05-21 12:00:06 -0400

    Decouple message state from actual text.

--- GameState.hs
+++ GameState.hs
@@ -7,29 +7,31 @@ import TerrainView
 data GameState = GameState { gsMap :: GameMap
                            , gsPlayer :: Position
                            , gsOrc :: Position
-                           , gsMessage :: String
+                           , gsMessage :: GameMessage
                            }
                | QuitGame deriving (Show)
+data GameMessage = NoMessage | SeeOrc deriving (Eq, Show)
 type PositionGetter = GameState -> Position
 type PositionSetter = GameState -> Position -> GameState
 
 newGame = GameState { gsMap = mapWithRoom (35,7) (44,16)
                     , gsPlayer = (42, 9)
                     , gsOrc = (37, 14)
-                    , gsMessage = "You see a horrible orc!"
+                    , gsMessage = SeeOrc
                     }
 
 instance Roguelike GameState where
     isOver QuitGame = True
     isOver gameState = False
-    viewMessage = gsMessage
+    viewMessage gameState | gsMessage gameState == NoMessage = ""
+                          | gsMessage gameState == SeeOrc = "You see a horrible orc!"
     viewTile position gameState | gsPlayer gameState == position = '@'
     viewTile position gameState | gsOrc gameState == position = 'o'
     viewTile position GameState { gsMap = gameMap } = viewTerrain position gameMap
     viewTiles gameState = [(x, y, viewTile (x,y) gameState) | (x,y) <- positionsOnMap $ gsMap gameState]
 
-setPlayer gameState player = gameState { gsPlayer = player, gsMessage = "" }
-setOrc gameState orc = gameState { gsOrc = orc, gsMessage = "" }
+setPlayer gameState player = gameState { gsPlayer = player, gsMessage = NoMessage }
+setOrc gameState orc = gameState { gsOrc = orc, gsMessage = NoMessage }
 
 walkableTerrain position gameMap = terrainAt position gameMap == Floor
 unoccupied position gameState = (gsOrc gameState) /= position
