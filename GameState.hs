27c1154b87c8c79bf708c7f3a80939c36d6ab131
Moss Collum <moss.collum@gmail.com>
2014-06-24 08:27:49 -0700

    Clean up code, maybe? Augh! Is this helping?!

--- GameState.hs
+++ GameState.hs
@@ -23,20 +23,33 @@ newGame = GameState { gsMap = mapWithRoom (35,7) (44,16)
                     , gsMessage = SeeOrc
                     }
 
+-- | A GameState is viewable as a Roguelike
+-- >>> let gs = newGame
+-- >>> viewTile (42, 9) gs
+-- '@'
+-- >>> viewTile (37, 14) gs
+-- 'o'
+-- >>> viewTile (35, 7) gs
+-- '.'
 instance Roguelike GameState where
     isOver QuitGame = True
     isOver gameState = False
-    viewMessage gameState | gsMessage gameState == NoMessage = ""
-                          | gsMessage gameState == SeeOrc = "You see a horrible orc!"
-                          | gsMessage gameState == OrcHits = "The orc stabs you with a dagger!"
+    viewMessage GameState { gsMessage=m }
+                  | m == NoMessage = ""
+                  | m == SeeOrc = "You see a horrible orc!"
+                  | m == OrcHits = "The orc stabs you with a dagger!"
     viewStatus gameState = "HP: " ++ (show $ cHitPoints $ gsPlayer gameState)
-    viewTile position gameState | cPosition (gsPlayer gameState) == position = '@'
-    viewTile position gameState | cPosition (gsOrc gameState) == position = 'o'
-    viewTile position GameState { gsMap = gameMap } = viewTerrain position gameMap
+    viewTile position gameState
+           | position == playerPosition gameState = '@'
+           | position == orcPosition gameState = 'o'
+           | True = viewTerrain position $ gsMap gameState
     viewTiles gameState = [(x, y, viewTile (x,y) gameState) | (x,y) <- positionsOnMap $ gsMap gameState]
 
 setPlayer player gameState = gameState { gsPlayer = player, gsMessage = NoMessage }
 setOrc orc gameState = gameState { gsOrc = orc, gsMessage = NoMessage }
+
+playerPosition = cPosition . gsPlayer
+orcPosition = cPosition . gsOrc
 
 walkableTerrain position gameMap = terrainAt position gameMap == Floor
 
@@ -44,5 +57,5 @@ occupied :: Position -> GameState -> Bool
 occupied position gameState = not $ unoccupied position gameState
 
 unoccupied :: Position -> GameState -> Bool
-unoccupied position gameState = (cPosition $ gsOrc gameState) /= position
-                              && (cPosition $ gsPlayer gameState) /= position
+unoccupied position gameState = (orcPosition gameState) /= position
+                              && (playerPosition gameState) /= position
